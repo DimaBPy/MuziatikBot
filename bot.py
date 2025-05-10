693c017f75e153bf2d80f72a000f11ccacccfd6a
dimabarnevdb <dimabarnevdb@gmail.com>
2025-05-10 14:06:19 +0000

    bot v1.5

--- bot.py
+++ bot.py
@@ -3,23 +3,31 @@ import os
 import asyncio
 import telegram
 from aiogram import Bot, Dispatcher, F, Router, types
-from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.filters import Command
 from dotenv import load_dotenv
 
 load_dotenv()
-beta_keyboard = ReplyKeyboardMarkup(keyboard=[
-    [KeyboardButton(text='Поставь напоминание'),
-     KeyboardButton(text='Котики!')],
-    [KeyboardButton(text='Как меня зовут?'),
-     KeyboardButton(text='Домик')],
-    [KeyboardButton(text='Отзыв')]
+
+settings_keyboard = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text='Выбрать имя', callback_data='name')]
+])
+
+dev_keyboard = ReplyKeyboardMarkup(keyboard=[
+    [KeyboardButton(text='Кубик')],
+    [KeyboardButton(text="Info", )],
+    [KeyboardButton(text='Поставь напоминание (no)'),
+     KeyboardButton(text='Котики! (later)')],
+    [KeyboardButton(text='Как меня зовут? (changed)'),
+     KeyboardButton(text='Домик (was good)')],
+    [KeyboardButton(text='Отзыв (need it)')]
 ],
     resize_keyboard=True)
 
 keyboard = ReplyKeyboardMarkup(keyboard=[
     [KeyboardButton(text='Кубик')],
-    [KeyboardButton(text="Info", )]
+    [KeyboardButton(text="Info", )],
+    [KeyboardButton(text='Настройки (beta)')],
 ], resize_keyboard=True)
 
 api_token_muziatikbot = os.getenv("API_TOKEN_muziatikBot")
@@ -31,7 +39,7 @@ router = Router()
 
 @router.message(Command('start'))
 async def start_bot(message: Message):
-    await message.answer("Здравствуйте, я **MuziatikBot**. Нажмите на _кнопку_ внизу, чтобы узнать больше о мне.",
+    await message.answer("Здравствуйте, я **MuziatikBot**. Нажмите на _кнопку_ внизу, чтобы узнать больше обо мне.",
                          parse_mode="Markdown", reply_markup=ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='Info')]], resize_keyboard=True))
 
 
@@ -41,25 +49,17 @@ async def info(message: Message, bot: Bot):
                                        '933qZ43mWlN1MK_QjAACsQ8AAldGSEutS54Fv2EAAe42BA')
     await asyncio.sleep(3)
     await message.reply('Вот infoрмаация о MuziatikBot:\n'
-                        'Версия — 1.3\nДоступность фнкций: Возможности Bot_v1 — недоступно,\n Кубик: Брось кубик — можно пробовать.',
+                        'Версия — 1.5\nДоступность фнкций: Возможности Bot_v1 — начальная стадия,\n Кубик — публичный предпросмотр.',
                         reply_markup=keyboard)
-
-
-@router.message(lambda msg: msg.text == 'Отзыв')
-async def feedback(message: Message):
-    await message.reply(
-        '_Напишите_ Ваш отзыв (функция неработает)',
-        parse_mode="Markdown",
-        reply_markup=keyboard)
 
 
 @router.message(lambda msg: "Кубик" in msg.text)
 async def dice(message: Message, bot: Bot):
-    inline_keyboard = types.InlineKeyboardMarkup(inline_keyboard=[
-        [types.InlineKeyboardButton(
+    inline_keyboard = InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(
             text="Брось кубик (стикер)", callback_data="roll_dice")],
-        [types.InlineKeyboardButton(
-            text='Рандомное число (текст)', callback_data='text_dice')]
+        [InlineKeyboardButton(
+            text='Случайное число (текст)', callback_data='text_dice')]
     ])
     await message.answer("Выберите вариант:", reply_markup=inline_keyboard)
 
@@ -73,6 +73,35 @@ async def roll_dice(callback_query: types.CallbackQuery, bot: Bot):
 async def text_dice(callback_query: types.CallbackQuery, bot: Bot):
     dice_result = random.randint(1, 6)
     await bot.send_message(callback_query.from_user.id, f"Выпало: {dice_result}")
+
+
+@router.message(lambda msg: msg.text == 'Настройки (beta)')
+async def settings(message: Message):
+    global set
+    set = await message.reply('Здесь будут настройки', reply_markup=settings_keyboard)
+
+
+@router.callback_query(lambda c: c.data == 'name')
+async def name(callback_query: types.CallbackQuery, bot: Bot):
+    print(f'{set=}')
+    await bot.edit_message_reply_markup(set.chat.id, set.message_id, reply_markup=InlineKeyboardMarkup(
+        inline_keyboard=[
+            [InlineKeyboardButton(text='ФИ')]
+        ]
+    ))
+
+
+@router.message(lambda msg: msg.text == 'dev')
+async def dev(message: Message):
+    await message.reply('Okei-dokei', reply_markup=dev_keyboard)
+
+
+@router.message(lambda msg: msg.text == 'Отзыв')
+async def feedback(message: Message):
+    await message.reply(
+        '_Напишите_ Ваш отзыв (функция неработает)',
+        parse_mode="Markdown",
+        reply_markup=keyboard)
 
 
 async def main():
